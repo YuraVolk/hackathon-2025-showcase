2ecc4a7318fe7609309c1f314aef48467185388e
Yuriy <y.volkovsky@friflex.com>
2025-04-23 14:41:19 +0300

    add sorting

--- src/pages/partners-dashboard/index.tsx
+++ src/pages/partners-dashboard/index.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/Header/Header";
 import Image from "next/image";
 
 import styles from "./styles.module.css";
@@ -6,13 +5,60 @@ import logoIcon from "../../assets/icons/logo.svg";
 
 import { Volunteer } from "@/components/Volunteer/Volunteer";
 import { VOLUNTEERS } from "@/data/volunteers";
+import { Filters } from "@/components/Filters/Filters";
+import { Layout } from "@/components/Layout/Layout";
+import { useMemo, useState } from "react";
+import { Sorting } from "@/data/filters";
+import dayjs, { Dayjs } from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
 
 const PartnersDashboard = () => {
+  const [achievements, setAchievements] = useState<string[]>([]);
+  const [sorting, setSorting] = useState(Sorting.ByAchievementsAscending);
+  const [minBirthDate, setMinBirthDate] = useState<Dayjs | null>(null);
+  const [maxBirthDate, setMaxBirthDate] = useState<Dayjs | null>(null);
+
+  const achievementTypes = useMemo(
+    () =>
+      Array.from(
+        new Set(VOLUNTEERS.map((volunteer) => volunteer.achievements))
+      ),
+    []
+  );
+
+  const filteredVolunteers = useMemo(() => {
+    const baseVolunteers = VOLUNTEERS.filter((volunteer) => {
+      if (
+        achievements.length !== 0 &&
+        !achievements.includes(volunteer.achievements)
+      ) {
+        return false;
+      }
+
+      const birthDay = dayjs(volunteer.birth_date, "DD.MM.YYYY");
+      if (
+        (minBirthDate !== null && birthDay.isBefore(minBirthDate)) ||
+        (maxBirthDate !== null && birthDay.isAfter(maxBirthDate))
+      ) {
+        return false;
+      }
+
+      return true;
+    });
+
+    if (sorting === Sorting.ByAchievementsDescending) {
+      baseVolunteers.reverse();
+    }
+
+    return baseVolunteers;
+  }, [achievements, sorting, minBirthDate, maxBirthDate]);
+
   return (
-    <>
-      <Header
-        logo={<Image src={logoIcon} alt="Лого" width={32} height={32} />}
-        categories={[
+    <Layout
+      headerProps={{
+        logo: <Image src={logoIcon} alt="Лого" width={32} height={32} />,
+        categories: [
           {
             title: "Главная",
             url: "/",
@@ -21,7 +67,10 @@ const PartnersDashboard = () => {
             title: "Волонтеры",
             url: "/partners-dashboard/",
             childCategories: [
-              { title: "Список бонусов", url: "/partners-dashboard/bonuses/" },
+              {
+                title: "Список бонусов",
+                url: "/partners-dashboard/bonuses/",
+              },
               {
                 title: "Регистрация бонуса",
                 url: "/partners-dashboard/bonuses/new",
@@ -30,18 +79,35 @@ const PartnersDashboard = () => {
           },
           {
             title: "Связаться с нами",
-            url: () => window.scrollTo(0, document.body.scrollHeight),
+            url: () =>
+              window.scrollTo({
+                left: 0,
+                top: document.body.scrollHeight,
+                behavior: "smooth",
+              }),
           },
-        ]}
+        ],
+      }}
+    >
+      <Filters
+        achievementTypes={achievementTypes}
+        achievements={achievements}
+        setAchievements={setAchievements}
+        sorting={sorting}
+        setSorting={setSorting}
+        minBirthDate={minBirthDate}
+        setMinBirthDate={setMinBirthDate}
+        maxBirthDate={maxBirthDate}
+        setMaxBirthDate={setMaxBirthDate}
       />
       <ul className={styles.volunteers}>
-        {VOLUNTEERS.map((volunteer) => (
-          <li key={volunteer.id}>
+        {filteredVolunteers.map((volunteer) => (
+          <li>
             <Volunteer volunteer={volunteer} />
           </li>
         ))}
       </ul>
-    </>
+    </Layout>
   );
 };
 
